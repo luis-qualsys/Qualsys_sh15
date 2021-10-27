25d2884abe52612fc92ef25add176b526e599f06
david-qualsys <david_mejia@qualsys.com.mx>
2021-10-27 17:14:26 -0500

    [FIX] JS para Odoo 15

--- fer_barcode_validation/__manifest__.py
+++ fer_barcode_validation/__manifest__.py
@@ -14,11 +14,8 @@
     'author': "Qualsys Consulting",
     'website': "https://www.qualsys.com.mx",
 
-    # Categories can be used to filter modules in modules listing
-    # Check https://github.com/odoo/odoo/blob/13.0/odoo/addons/base/data/ir_module_category_data.xml
-    # for the full list
     'category': 'Purchase',
-    'version': '0.1',
+    'version': '15.0.1',
 
     # any module necessary for this one to work correctly
     'depends': ['base', 
@@ -30,8 +27,13 @@
     # always loaded
     'data': [
         'security/ir.model.access.csv',
-        'static/src/xml/bar_validation.xml',
     ],
+    'assets': {
+        'stock_barcode.assets_backend': [
+            '/fer_barcode_validation/static/src/js/bar_validation_line.js',
+            '/fer_barcode_validation/static/src/js/bar_validation_client.js',
+        ]
+    },
     # only loaded in demonstration mode
     'demo': [],
 }
